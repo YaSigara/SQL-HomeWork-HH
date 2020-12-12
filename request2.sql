cef2412fc0eb66843e4f516aff0fd3429f413947
Сергей Шигоирн <shigorin_s@mail.ru>
2020-12-12 21:43:21 +0300

    Update request2.sql

--- request2.sql
+++ request2.sql
@@ -1,16 +1,21 @@
 SET search_path=jobfinder;
 SELECT
     AVG(
-CASE WHEN(compensation_gross IS TRUE)
-THEN compensation_from
-ELSE compensation_from*0.87 END)
-AS min_avg_compensation,
+    CASE 
+        WHEN(compensation_gross IS TRUE)
+        THEN compensation_from
+        ELSE compensation_from*0.87 
+        END) AS min_avg_compensation,
     AVG(
-CASE WHEN (compensation_gross IS TRUE)
-THEN compensation_to
-ELSE compensation_to*0.87 END) AS max_avg_compensation,
+    CASE 
+        WHEN (compensation_gross IS TRUE)
+        THEN compensation_to
+        ELSE compensation_to*0.87 
+        END) AS max_avg_compensation,
     AVG(
-CASE WHEN(compensation_gross IS TRUE)
-THEN ((compensation_to + compensation_from)/2)
-ELSE ((compensation_to + compensation_from)/2)*0.87 END) AS mid_avg_compensation
+    CASE 
+        WHEN(compensation_gross IS TRUE)
+        THEN ((compensation_to + compensation_from)/2)
+        ELSE ((compensation_to + compensation_from)/2)*0.87 
+        END) AS mid_avg_compensation
 FROM vacancy;
