047312f7bfbac87d89a1a9809725c82e8c89d75b
Сергей Шигоирн <shigorin_s@mail.ru>
2020-12-12 21:30:45 +0300

    Update request2.sql

--- request2.sql
+++ request2.sql
@@ -3,14 +3,14 @@ SELECT
     AVG(
 CASE WHEN(compensation_gross IS TRUE)
 THEN compensation_from
-ELSE compensation_from/87*100 END)
+ELSE compensation_from*0.87 END)
 AS min_avg_compensation,
     AVG(
 CASE WHEN (compensation_gross IS TRUE)
 THEN compensation_to
-ELSE compensation_to / 87 * 100 END) AS max_avg_compensation,
+ELSE compensation_to*0.87 END) AS max_avg_compensation,
     AVG(
 CASE WHEN(compensation_gross IS TRUE)
 THEN ((compensation_to + compensation_from)/2)
-ELSE ((compensation_to + compensation_from)/2)/87*100 END) AS mid_avg_compensation
+ELSE ((compensation_to + compensation_from)/2)*0.87 END) AS mid_avg_compensation
 FROM vacancy;
