1ffdb98c93573b017ee04f4e7ad1108587da4841
Сергей Шигоирн <shigorin_s@mail.ru>
2020-12-26 20:16:49 +0300

    Update request1.sql

--- request1.sql
+++ request1.sql
@@ -1,12 +1,11 @@
-SET search_path=jobfinder;
-SELECT post.post_name,
-       area.area_name,
-       employer.org_name
-FROM post
-INNER JOIN vacancy ON post.post_id = vacancy.post_id
-INNER JOIN area ON area.area_id=vacancy.aria_id
+SELECT post AS vacancy_name,
+       area.name AS area,
+       employer.org_name AS employer
+FROM vacancy
+INNER JOIN area ON area.area_id=vacancy.area_id
 INNER JOIN employer ON employer.employer_id=vacancy.employer_id
 WHERE vacancy.compensation_from IS NULL
 AND vacancy.compensation_to IS NULL
-ORDER BY vacancy.created_at
+ORDER BY
+         vacancy.created_at
 LIMIT 10;
