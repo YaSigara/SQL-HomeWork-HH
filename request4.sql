202ad381fcca7c187f92386d77f4e69e6731473c
Сергей Шигоирн <shigorin_s@mail.ru>
2020-12-26 20:30:27 +0300

    Update request4.sql

--- request4.sql
+++ request4.sql
@@ -1,7 +1,15 @@
 SET search_path=jobfinder;
+WITH vacancy_counter AS (
+    SELECT
+        vacancy.employer_id,
+        count(vacancy.vacancy_id) AS vacancy_count
+    FROM employer
+    INNER JOIN vacancy ON vacancy.employer_id = employer.employer_id
+    GROUP BY
+             vacancy.employer_id
+    ORDER BY
+             vacancy_count
+)
 SELECT
-    employer.org_name AS company,
-    percentile_cont(0.5) WITHIN GROUP ( ORDER BY vacancy_id ) AS median_count
-FROM vacancy
-INNER JOIN employer ON vacancy.employer_id = employer.employer_id
-GROUP BY company;
+    percentile_cont(0.5) WITHIN GROUP (ORDER BY vacancy_count) AS median_count
+FROM vacancy_counter;
