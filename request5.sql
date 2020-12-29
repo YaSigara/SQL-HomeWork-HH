00822f9f1699a5bdf35f1ffe9fce7be668d83e18
Сергей Шигоирн <shigorin_s@mail.ru>
2020-12-29 14:58:47 +0300

    Update request5.sql

--- request5.sql
+++ request5.sql
@@ -1,23 +1,23 @@
-SET search_path=jobfinder;
 WITH first_response AS (
     SELECT
         area.area_id AS city,
-        MIN(response.created_at - vacancy.created_at) OVER (PARTITION BY vacancy.area_id) AS min_interval
+        MIN(response.created_at - vacancy.created_at) OVER (PARTITION BY response.vacancy_id) AS min_response
     FROM vacancy
     INNER JOIN response ON vacancy.vacancy_id = response.vacancy_id
     RIGHT JOIN area ON vacancy.area_id = area.area_id
-    GROUP BY city,
-             response.created_at,
+    GROUP BY
+             city,
+             response.vacancy_id,
              vacancy.created_at,
-             vacancy.area_id)
+             response.created_at
+             )
 SELECT
-        area.name as region,
-        coalesce(MIN(first_response.min_interval),'0') AS min_interval,
-        coalesce(MAX(first_response.min_interval),'0') AS max_interval
+        city,
+        coalesce(MIN(first_response.min_response),'0') AS min_interval,
+        coalesce(MAX(first_response.min_response),'0') AS max_interval
 FROM first_response
 RIGHT JOIN area ON area.area_id=first_response.city
 GROUP BY
-         region,
          city
 ORDER BY
-         region;
+         city;
